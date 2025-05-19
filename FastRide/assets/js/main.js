34f9e004e518d3d1dcf790e9c39f443d53fad4ee
chief <chief.vinograd@yandex.ru>
2025-05-19 20:03:09 +0200

    added register logic

--- FastRide/assets/js/main.js
+++ FastRide/assets/js/main.js
@@ -25,3 +25,28 @@ document.getElementById('login-form').addEventListener('connexion', function(eve
     alert('Invalid email or password');
   }
 });
+
+document.getElementById('register-form').addEventListener('inscription', function(event) {
+  event.preventDefault(); // Prevent the default form submission
+
+  const email = document.getElementById('new-email').value;
+  const password = document.getElementById('new-password').value;
+  const passwordConfirmation = document.getElementById('new-password-confirmation').value;
+  const name = document.getElementById('new-name').value;
+
+  const response = fetch('server', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password, passwordConfirmation, name })
+  })
+
+  const result = response.json();
+  if(response.ok) {
+    localStorage.setItem('token', result.token);
+    window.location.href = 'login.html';
+  } else {
+    alert('Registration failed');
+  }
+});
