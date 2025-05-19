d37872e28393c7afa4ef0936735ddd4fa7569124
chief <chief.vinograd@yandex.ru>
2025-05-19 21:29:30 +0200

    added vue covoiturages

--- FastRide/assets/js/main.js
+++ FastRide/assets/js/main.js
@@ -8,13 +8,14 @@ document.getElementById('login-form').addEventListener('connexion', function(eve
   
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
+  const userType = document.getElementById('userType').value;
 
   const response = fetch('server', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({ email, password })
+    body: JSON.stringify({ email, password, userType })
   })
 
   const result = response.json();
