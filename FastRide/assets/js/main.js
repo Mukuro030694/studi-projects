d6086117a781b2f6d2acf38dba44742196ebd634
Mukuro030694 <152009361+Mukuro030694@users.noreply.github.com>
2025-05-20 19:38:18 +0200

    Merge test into dev (#1)
    * position of the text fixed in footer
    
    * added glow effect to the arrows
    
    * merged the text and the image boxes. Added carousel for images.
    
    * created login/reg form
    
    * added login logic
    
    * added register logic
    
    * added the contact page
    
    * added vue covoiturages
    
    * added photos for the drivers cards
    
    ---------
    
    Co-authored-by: chief <chief.vinograd@yandex.ru>

--- FastRide/assets/js/main.js
+++ FastRide/assets/js/main.js
@@ -2,3 +2,52 @@ function toggleMenu() {
   const menu = document.getElementById("sideMenu");
   menu.classList.toggle("active");
 }
+
+document.getElementById('login-form').addEventListener('connexion', function(event) {
+  event.preventDefault();
+  
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const userType = document.getElementById('userType').value;
+
+  const response = fetch('server', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password, userType })
+  })
+
+  const result = response.json();
+  if(response.ok) {
+    localStorage.setItem('token', result.token);
+    window.location.href = 'login.html';
+  } else {
+    alert('Invalid email or password');
+  }
+});
+
+document.getElementById('register-form').addEventListener('inscription', function(event) {
+  event.preventDefault();
+
+  const email = document.getElementById('new-email').value;
+  const password = document.getElementById('new-password').value;
+  const passwordConfirmation = document.getElementById('new-password-confirmation').value;
+  const name = document.getElementById('new-name').value;
+
+  const response = fetch('server', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({ email, password, passwordConfirmation, name })
+  })
+
+  const result = response.json();
+  if(response.ok) {
+    localStorage.setItem('token', result.token);
+    window.location.href = 'login.html';
+  } else {
+    alert('Registration failed');
+  }
+});
